643acf25b786152d620edfbc98e90bb482cc5bdf
lorenzo-lavacchini <lorenzo.lavacchini@gmail.com>
2023-06-10 17:03:14 +0200

    Adjustements to login and logout

--- PPM_Project/settings.py
+++ PPM_Project/settings.py
@@ -132,3 +132,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = 'recipes-home'
+
+LOGIN_URL = 'user-login'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from . import forms
 # Create your views here.
 def register(request):
@@ -13,3 +14,7 @@ def register(request):
     else:
         form = forms.UserRegisterForm()
     return render(request, 'users/register.html',{'form':form})
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
