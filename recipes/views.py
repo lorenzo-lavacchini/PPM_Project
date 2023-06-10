7891f4492009303e1abc26c0d592e9848445e994
lorenzo-lavacchini <lorenzo.lavacchini@gmail.com>
2023-06-10 09:35:25 +0200

    Fist modifications to views

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,32 @@
 
 from django.shortcuts import render, HttpResponse
 # Create your views here.
+
+recipes=[
+    {
+    'author':'Dom',
+    'title':'meatball sub',
+    'directions':'combine all ingredients',
+    'date_posted':'May 19, 2022'
+    },
+    {
+    'author':'Dom',
+    'title':'turkey sub',
+    'directions':'combine all ingredients',
+    'date_posted':'May 16, 2022'
+    },
+    {
+    'author':'Dom',
+    'title':'ham and cheese sub',
+    'directions':'combine all ingredients',
+    'date_posted':'May 19, 2022'
+    },
+]
 def home(request):
-    return HttpResponse("<h1>Welcome to my recipe App<h1>")
+    context ={
+        'recipes':recipes
+    }
+    return render(request, "recipes/home.html", context)
 
 def about(request):
-    return HttpResponse("<h1>This is a recipes app to keep track of yout recipes")
+    return render(request, "recipes/about.html", {'title':'about us page'})
