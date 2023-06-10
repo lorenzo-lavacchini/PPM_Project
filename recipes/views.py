f128332914cb09a801682cef39c1df398b8b9e7e
lorenzo-lavacchini <lorenzo.lavacchini@gmail.com>
2023-06-10 08:41:20 +0200

    Added first Views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
+from django.shortcuts import render, HttpResponse
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1>Welcome to my recipe App<h1>")
+
+def about(request):
+    return HttpResponse("<h1>This is a recipes app to keep track of yout recipes")
