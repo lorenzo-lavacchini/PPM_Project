da470e1e21ab297130033f7c946e93ee281fe0ef
lorenzo-lavacchini <lorenzo.lavacchini@gmail.com>
2023-06-10 14:53:50 +0200

    Added custom form for registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from . import forms
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = forms.UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,username+"account created")
+            return redirect('recipes-home')
+    else:
+        form = forms.UserRegisterForm()
+    return render(request, 'users/register.html',{'form':form})
