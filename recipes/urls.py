8c0db32c96c340f0acaa826710d5495cf1c3939a
lorenzo-lavacchini <lorenzo.lavacchini@gmail.com>
2023-06-11 09:32:08 +0200

    Added functionalities for updating and deleting recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,9 @@ from . import views
 urlpatterns = [
     path('', views.RecipeListView.as_view(), name="recipes-home"),
     path('recipe/create/', views.RecipeCreateView.as_view(), name="recipes-create"),
+    path('recipe/<int:pk>/update/', views.RecipeUpdateView.as_view(), name="recipes-update"),
     path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name="recipes-detail"),
+    path('recipe/<int:pk>/delete/', views.RecipeDeleteView.as_view(), name="recipes-delete"),
     path('about/', views.about, name="recipes-about"),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from . import models
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -26,6 +27,23 @@ class RecipeCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+class RecipeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = models.Recipe
+    fields = ['title','description']
+
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.author
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class RecipeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = models.Recipe
+    success_url = reverse_lazy('recipes-home')
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.author
 def about(request):
     return render(request, "recipes/about.html", {'title':'about us page'})
 
